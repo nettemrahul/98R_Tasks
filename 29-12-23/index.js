bbb00f8948eb378028948edc4c34b1c9a49cd0a3
= <=>
2024-01-03 15:58:56 +0530

    Added a new file

---
+++ 29-12-23/index.js
@@ -0,0 +1,29 @@
+// * Pattern
+console.log("* Pattern");
+for(var i = 4; i >=1; i-- ) {
+    var line = "";
+    for(var j = 1; j <=i; j++){
+        
+        line += "*";
+    }
+        console.log(line);
+   
+     
+
+}
+
+
+// * Reverse Pattern 
+console.log("Reverse * Pattern");
+for(var i = 1; i <=4; i++) {
+    var line = "";
+    for(var j = 1; j <=i; j++){
+        
+        line += "*";
+    }
+        console.log(line);
+   
+     
+
+}
+
